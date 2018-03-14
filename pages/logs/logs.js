8ed1588c9e40f1f20f09d135c39b382a24d3ed20
myqs <184328322@qq.com>
2018-03-14 09:45:53 +0800

    分支

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -6,10 +6,7 @@ Page({
         logs: []
     },
     onLoad: function () {
-        this.setData({
-            logs: (wx.getStorageSync('logs') || []).map(log = > {
-                return util.formatTime(new Date(log))
-            })
-    })
+
+
     }
 })
